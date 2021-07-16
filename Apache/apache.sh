bdf23ed9226cc00e7d97e4447bc842b180681ac4
asgaralipq <asgaralipq@gmail.com>
2021-07-16 19:10:55 +0530

    Vim and Apache

---
+++ Apache/apache.sh
@@ -0,0 +1,23 @@
+   30  sudo apt update
+   31  less /etc/services 
+   32  history
+   33  ls -ltra
+   34  less .bashrc
+   35  vi --version
+   36  cd
+   37  pwd
+   38  cp /etc/services testfile
+   39  ls
+   40  vim testfile
+   41  joe testfile
+   42  vim testfile
+   43  sudo apt install apache2
+   44  sudo systemctl stop apache2
+   45  sudo systemctl start apache2
+   46  less /etc/apache2/apache2.conf 
+   47  sudo apt upgrade
+   48  less /var/log/apache2/access.log 
+   49  less /var/log/apache2/error.log 
+   50  ls
+   51  history 
+
